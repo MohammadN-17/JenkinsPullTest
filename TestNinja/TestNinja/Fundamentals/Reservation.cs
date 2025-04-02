7e28742b1c21da73529da26357fe28368b6d2343
Mohammad Nassrallah <mohammadnassrallah17@gmail.com>
2025-04-02 10:16:58 -0400

    test commit

--- TestNinja/TestNinja/Fundamentals/Reservation.cs
+++ TestNinja/TestNinja/Fundamentals/Reservation.cs
@@ -6,7 +6,9 @@
 
         public bool CanBeCancelledBy(User user)
         {
-            return (user.IsAdmin || MadeBy == user);
+            return (user.IsAdmin || 
+                
+                MadeBy == user);
         }
         
     }
